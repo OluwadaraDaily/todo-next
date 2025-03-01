cd576d2d7f1d835d48e1a1f921edefda9a2e7b16
Oluwadara Oloye <daraoloye99@gmail.com>
2025-03-01 18:58:19 +0100

    feature: fetch all tasks

--- src/app/api/tasks/route.ts
+++ src/app/api/tasks/route.ts
@@ -1,23 +1,39 @@
 import { prisma } from "@/lib/prisma";
-import { NextApiResponse, NextApiRequest } from "next";
+import { NextResponse, NextRequest } from "next/server";
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
-  const tasks = await prisma.task.findMany();
-  console.log('TASKS =>', tasks);
-
-  res.status(200).json(tasks);
+export async function GET(req: Request) {
+  try {
+    console.log('GET TASKS');
+    const tasks = await prisma.task.findMany();
+    console.log('TASKS =>', tasks);
+    if (!tasks) {
+      return NextResponse.json({
+        error: "No tasks found"
+      }, { status: 404 });
+    }
+    const serializedTasks = tasks.map((task) => ({
+      ...task,
+      id: task.id.toString(), // Convert BigInt to string
+    }));
+    return NextResponse.json(serializedTasks, {  status: 200 });
+  } catch (error: any) {
+    console.log('ERROR =>', error)
+    return NextResponse.json({
+      error: error.message || "Something went wrong"
+    }, { status: 500 });
+  }
 }
 
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
+export async function POST(req: NextRequest, res: NextResponse) {
   try {
-    const { body: data } = await req;
+    const data = await req.json();
     console.log('BODY [ADD TASKS] =>', data);
     const task = await prisma.task.create({ data });
     console.log('TASK =>', task);
-    res.status(201).json(task);
+    return NextResponse.json(task, {  status: 201 });
   } catch (error) {
-    res.status(500).json({
+    return NextResponse.json({
       error: "Something went wrong"
-    })
+    }, {  status: 500 });
   }
 }
